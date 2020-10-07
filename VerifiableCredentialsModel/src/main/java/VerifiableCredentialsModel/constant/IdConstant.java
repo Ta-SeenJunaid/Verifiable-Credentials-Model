a16ca7e9f9327e8a5d2cf0cd973351c88e629cc0
Ta-SeenJunaid <taseen.junaid@gmail.com>
2020-10-07 21:37:29 +0600

    IdConstant.java

---
+++ VerifiableCredentialsModel/src/main/java/VerifiableCredentialsModel/constant/IdConstant.java
@@ -0,0 +1,8 @@
+package VerifiableCredentialsModel.constant;
+
+public class IdConstant {
+	
+    public static final String UUID_PATTERN =
+            "[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$";
+
+}
