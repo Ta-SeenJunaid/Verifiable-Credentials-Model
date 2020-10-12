42073d4cee5c33826e8536399f8fda8ac27a38a2
Ta-SeenJunaid <taseen.junaid@gmail.com>
2020-10-12 22:11:51 +0600

    IdPrivateKey.java

---
+++ VerifiableCredentialsModel/src/main/java/VerifiableCredentialsModel/protocol/base/IdPrivateKey.java
@@ -0,0 +1,22 @@
+package VerifiableCredentialsModel.protocol.base;
+
+public class IdPrivateKey {
+	
+	private String  privateKey;
+
+	public IdPrivateKey() {
+		super();
+	}
+	
+	public IdPrivateKey(String privateKey) {
+		this.privateKey = privateKey;
+	}
+
+	public String getPrivateKey() {
+		return privateKey;
+	}
+
+	public void setPrivateKey(String privateKey) {
+		this.privateKey = privateKey;
+	}
+}
