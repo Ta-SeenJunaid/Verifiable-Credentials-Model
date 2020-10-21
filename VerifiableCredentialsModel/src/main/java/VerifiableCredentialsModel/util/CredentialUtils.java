39ae39099e47ab63e334bccf3ebf849bb9c2061b
Ta-SeenJunaid <taseen.junaid@gmail.com>
2020-10-21 16:55:18 +0600

    isCreateCredentialArgsValid

--- VerifiableCredentialsModel/src/main/java/VerifiableCredentialsModel/util/CredentialUtils.java
+++ VerifiableCredentialsModel/src/main/java/VerifiableCredentialsModel/util/CredentialUtils.java
@@ -2,7 +2,9 @@ package VerifiableCredentialsModel.util;
 
 import java.util.regex.Pattern;
 
+
 import VerifiableCredentialsModel.constant.CredentialConstant;
+import VerifiableCredentialsModel.constant.ErrorCode;
 import VerifiableCredentialsModel.constant.IdConstant;
 import VerifiableCredentialsModel.protocol.base.Credential;
 import VerifiableCredentialsModel.protocol.request.CreateCredentialArgs;
@@ -36,5 +38,43 @@ public class CredentialUtils {
 		return p.matcher(id).matches();
 	}
 	
+	public static ErrorCode isCreateCredentialArgsValid(
+			CreateCredentialArgs args) {
+		if (args == null) {
+			return ErrorCode.ILLEGAL_INPUT;
+		}
+		
+		if (args.getCptId() == null || args.getCptId().intValue() < 0) {
+			return ErrorCode.CPT_ID_ILLEGAL;
+		}
+		
+		///***********Have to check issuer Validity ********
+		
+		Long issuanceDate = args.getIssuanceDate();
+
+		if (issuanceDate != null && issuanceDate <= 0) {
+			return ErrorCode.CREDENTIAL_ISSUANCE_DATE_ILLEGAL;
+		}
+		Long expirationDate = args.getExpirationDate();
+		if (expirationDate == null
+				|| expirationDate.longValue() <0 
+				|| expirationDate.longValue() == 0) {
+			return ErrorCode.CREDENTIAL_EXPIRE_DATE_ILLEGAL;
+		}
+
+		if(!DateUtils.isAfterCurrentTime(expirationDate)) {
+			return ErrorCode.CREDENTIAL_CLAIM_NOT_EXISTS;
+		}
+		
+		if (issuanceDate != null && expirationDate < issuanceDate) {
+			return ErrorCode.CREDENTIAL_ISSUANCE_DATE_ILLEGAL;
+		}
+		
+		if (args.getClaim() == null || args.getClaim().isEmpty()) {
+			return ErrorCode.CREDENTIAL_CLAIM_NOT_EXISTS;
+		}
+		
+		return ErrorCode.SUCCESS;
+	}
 
 }
