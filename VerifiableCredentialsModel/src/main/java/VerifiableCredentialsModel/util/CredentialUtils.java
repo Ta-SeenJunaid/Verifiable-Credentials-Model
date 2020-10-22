b45b9f4fbc77f443990f3e4deba58dd572211a86
Ta-SeenJunaid <taseen.junaid@gmail.com>
2020-10-22 21:13:00 +0600

    isCredentialProofValid, isCredentialProofTypeValid, isCredentialContentValid

--- VerifiableCredentialsModel/src/main/java/VerifiableCredentialsModel/util/CredentialUtils.java
+++ VerifiableCredentialsModel/src/main/java/VerifiableCredentialsModel/util/CredentialUtils.java
@@ -1,11 +1,15 @@
 package VerifiableCredentialsModel.util;
 
+import java.util.Map;
 import java.util.regex.Pattern;
 
+import org.apache.commons.lang3.StringUtils;
 
 import VerifiableCredentialsModel.constant.CredentialConstant;
+import VerifiableCredentialsModel.constant.CredentialConstant.CredentialProofType;
 import VerifiableCredentialsModel.constant.ErrorCode;
 import VerifiableCredentialsModel.constant.IdConstant;
+import VerifiableCredentialsModel.constant.ParamKeyConstant;
 import VerifiableCredentialsModel.protocol.base.Credential;
 import VerifiableCredentialsModel.protocol.request.CreateCredentialArgs;
 
@@ -75,6 +79,67 @@ public class CredentialUtils {
 		}
 		
 		return ErrorCode.SUCCESS;
+	}
+	
+	public static ErrorCode isCredentialContentValid(Credential args) {
+		String credentialId = args.getId();
+		if (StringUtils.isEmpty(credentialId) || 
+				!CredentialUtils.isValidUuid(credentialId)) {
+			return ErrorCode.CREDENTIAL_ID_NOT_EXISTS;
+		}
+		String context = args.getContext();
+		if (StringUtils.isEmpty(context)) {
+			return ErrorCode.CREDENTIAL_CONTEXT_NOT_EXISTS;
+		}
+		Long issuanceDate = args.getIssuanceDate();
+		if (issuanceDate == null) {
+			return ErrorCode.CREDENTIAL_ISSUANCE_DATE_ILLEGAL;
+		}
+		
+		if (issuanceDate.longValue() > args.getExpirationDate().longValue()) {
+			return ErrorCode.CREDENTIAL_EXPIRED;
+		}
+		
+		Map<String, String> proof = args.getProof();
+		return isCredentialProofValid(proof);
+	}
+	
+	private static boolean isCredentialProofTypeValid(String type) {
+		
+		if (!StringUtils.isEmpty(type)) {
+			for (CredentialProofType proofType : CredentialConstant.CredentialProofType.values()) {
+				if(StringUtils.equalsIgnoreCase(type, proofType.getTypeName())) {
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+	
+	private static ErrorCode isCredentialProofValid(Map<String, String> proof) {
+		if (proof == null) {
+			return ErrorCode.ILLEGAL_INPUT;
+		}
+		String type = proof.get(ParamKeyConstant.PROOF_TYPE);
+		if(!isCredentialProofTypeValid(type)) {
+			return ErrorCode.CREDENTIAL_SIGNATURE_TYPE_ILLEGAL;
+		}
+		
+        Long created = Long.valueOf(proof.get(ParamKeyConstant.PROOF_CREATED));
+        if (created.longValue() <= 0) {
+            return ErrorCode.CREDENTIAL_ISSUANCE_DATE_ILLEGAL;
+        }
+        
+        //************* Have to work with creator ***************
+        
+        if (type.equalsIgnoreCase(CredentialProofType.ECDSA.getTypeName())) {
+        	String signature = proof.get(ParamKeyConstant.CREDENTIAL_SIGNATURE);
+        	if (StringUtils.isAllEmpty(signature) || !DataToolUtils.isValidBase64String(signature)) {
+        		return ErrorCode.CREDENTIAL_SIGNATURE_BROKEN;
+        	}
+        }
+        
+        return ErrorCode.SUCCESS;
 	}
 
 }
